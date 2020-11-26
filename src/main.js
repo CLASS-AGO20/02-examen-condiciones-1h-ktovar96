cef95795361547f9121bfe36ff20d22b9c020e47
Karla Tovar <ktovar@ucol.mx>
2020-11-26 16:57:13 -0600

    Implemented costoRenta

--- src/main.js
+++ src/main.js
@@ -11,9 +11,29 @@ export default class App {
         return dinero; 
     }
 
+    costoRenta(kilometros) {
+        let renta;
+        if (kilometros < 50){
+            renta= kilometros * 3;
+        } else if (kilometros  => 50 && kilometros < 100) {
+            renta= kilometros * 5;
+        } else if (kilometros => 100 && kilometros < 200){
+            renta= kilometros * 6;
+        } else {
+            renta= kilometros * 6.5;
+        }
+        return renta;
+    }
+
 }
 
 let app = new App();
  console.log ("Probando convertir");
  console.log (app.convertir(1500));
  console.log (app.convertir(2000));
+
+ console.log ("Probando costoRenta");
+ console.log (app.costoRenta(30));
+ console.log (app.costoRenta(50));
+ console.log (app.costoRenta(100));
+ console.log (app.costoRenta(250));
