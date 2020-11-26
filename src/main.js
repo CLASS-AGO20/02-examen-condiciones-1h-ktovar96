6263abcff6f5f05786c9cd838938c933bbdd6ea0
Karla Tovar <ktovar@ucol.mx>
2020-11-26 15:46:17 -0600

    Implemented convertir

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 export default class App {
+    convertir(pesos){
+        let dinero;
+
+        if (pesos < 2000) {
+            dinero= pesos / 20.5; 
+        } else {
+            dinero= pesos / 22.7;
+        }
+        return dinero; 
+    }
 
 }
 
 let app = new App();
+console.log ("Probando convertir");
+console.log(app.convertir(1500));
+console.log (app.convertir(2000));
